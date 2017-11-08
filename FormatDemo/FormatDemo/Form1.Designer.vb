ad7499b44e523535f7c38a6159b3a56c2cdbce53
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-11-08 10:23:04 -0600

    update

---
+++ FormatDemo/FormatDemo/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnNumberFormat = New System.Windows.Forms.Button()
+        Me.btnFixedPointFormat = New System.Windows.Forms.Button()
+        Me.btnExponetialFormat = New System.Windows.Forms.Button()
+        Me.btnCurrencyFormat = New System.Windows.Forms.Button()
+        Me.btnPercentFormat = New System.Windows.Forms.Button()
+        Me.btnShortDate = New System.Windows.Forms.Button()
+        Me.btnLongDate = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUserEntry = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnNumberFormat
+        '
+        Me.btnNumberFormat.Location = New System.Drawing.Point(0, 29)
+        Me.btnNumberFormat.Name = "btnNumberFormat"
+        Me.btnNumberFormat.Size = New System.Drawing.Size(122, 23)
+        Me.btnNumberFormat.TabIndex = 0
+        Me.btnNumberFormat.Text = "Number format (n)"
+        Me.btnNumberFormat.UseVisualStyleBackColor = True
+        '
+        'btnFixedPointFormat
+        '
+        Me.btnFixedPointFormat.Location = New System.Drawing.Point(0, 58)
+        Me.btnFixedPointFormat.Name = "btnFixedPointFormat"
+        Me.btnFixedPointFormat.Size = New System.Drawing.Size(122, 23)
+        Me.btnFixedPointFormat.TabIndex = 1
+        Me.btnFixedPointFormat.Text = "Fixed-point format (f)"
+        Me.btnFixedPointFormat.UseVisualStyleBackColor = True
+        '
+        'btnExponetialFormat
+        '
+        Me.btnExponetialFormat.Location = New System.Drawing.Point(0, 87)
+        Me.btnExponetialFormat.Name = "btnExponetialFormat"
+        Me.btnExponetialFormat.Size = New System.Drawing.Size(122, 23)
+        Me.btnExponetialFormat.TabIndex = 2
+        Me.btnExponetialFormat.Text = "Exponetial format (e)"
+        Me.btnExponetialFormat.UseVisualStyleBackColor = True
+        '
+        'btnCurrencyFormat
+        '
+        Me.btnCurrencyFormat.Location = New System.Drawing.Point(0, 116)
+        Me.btnCurrencyFormat.Name = "btnCurrencyFormat"
+        Me.btnCurrencyFormat.Size = New System.Drawing.Size(122, 23)
+        Me.btnCurrencyFormat.TabIndex = 3
+        Me.btnCurrencyFormat.Text = "Currency format(c)"
+        Me.btnCurrencyFormat.UseVisualStyleBackColor = True
+        '
+        'btnPercentFormat
+        '
+        Me.btnPercentFormat.Location = New System.Drawing.Point(0, 145)
+        Me.btnPercentFormat.Name = "btnPercentFormat"
+        Me.btnPercentFormat.Size = New System.Drawing.Size(122, 23)
+        Me.btnPercentFormat.TabIndex = 4
+        Me.btnPercentFormat.Text = "Percent format(p)"
+        Me.btnPercentFormat.UseVisualStyleBackColor = True
+        '
+        'btnShortDate
+        '
+        Me.btnShortDate.Location = New System.Drawing.Point(134, 29)
+        Me.btnShortDate.Name = "btnShortDate"
+        Me.btnShortDate.Size = New System.Drawing.Size(100, 23)
+        Me.btnShortDate.TabIndex = 5
+        Me.btnShortDate.Text = "Short date (d)"
+        Me.btnShortDate.UseVisualStyleBackColor = True
+        '
+        'btnLongDate
+        '
+        Me.btnLongDate.Location = New System.Drawing.Point(134, 58)
+        Me.btnLongDate.Name = "btnLongDate"
+        Me.btnLongDate.Size = New System.Drawing.Size(100, 23)
+        Me.btnLongDate.TabIndex = 6
+        Me.btnLongDate.Text = "Long date (D)"
+        Me.btnLongDate.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(134, 87)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 23)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Short time (t)"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(134, 116)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 23)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Long time (T)"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(134, 145)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(100, 23)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Full date/time (F)"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnNumberFormat)
+        Me.GroupBox1.Controls.Add(Me.Button10)
+        Me.GroupBox1.Controls.Add(Me.btnFixedPointFormat)
+        Me.GroupBox1.Controls.Add(Me.btnPercentFormat)
+        Me.GroupBox1.Controls.Add(Me.Button9)
+        Me.GroupBox1.Controls.Add(Me.btnShortDate)
+        Me.GroupBox1.Controls.Add(Me.Button8)
+        Me.GroupBox1.Controls.Add(Me.btnCurrencyFormat)
+        Me.GroupBox1.Controls.Add(Me.btnLongDate)
+        Me.GroupBox1.Controls.Add(Me.btnExponetialFormat)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 83)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(250, 191)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Select a Format"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Enter a number or date"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(49, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Formatted"
+        '
+        'txtUserEntry
+        '
+        Me.txtUserEntry.Location = New System.Drawing.Point(146, 20)
+        Me.txtUserEntry.Name = "txtUserEntry"
+        Me.txtUserEntry.Size = New System.Drawing.Size(158, 20)
+        Me.txtUserEntry.TabIndex = 13
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(113, 53)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(191, 23)
+        Me.lblResult.TabIndex = 14
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(278, 170)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 15
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(278, 199)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(365, 297)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtUserEntry)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Format Demo"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnNumberFormat As System.Windows.Forms.Button
+    Friend WithEvents btnFixedPointFormat As System.Windows.Forms.Button
+    Friend WithEvents btnExponetialFormat As System.Windows.Forms.Button
+    Friend WithEvents btnCurrencyFormat As System.Windows.Forms.Button
+    Friend WithEvents btnPercentFormat As System.Windows.Forms.Button
+    Friend WithEvents btnShortDate As System.Windows.Forms.Button
+    Friend WithEvents btnLongDate As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtUserEntry As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ FormatDemo/FormatDemo/Form1.vb
@@ -0,0 +1,111 @@
+﻿Public Class Form1
+
+    Private Sub btnNumberFormat_Click(sender As Object, e As EventArgs) Handles btnNumberFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserEntry.Text)
+
+        strResult = dblAnswer.ToString("n")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub btnFixedPointFormat_Click(sender As Object, e As EventArgs) Handles btnFixedPointFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserEntry.Text)
+
+        strResult = dblAnswer.ToString("f")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub btnExponetialFormat_Click(sender As Object, e As EventArgs) Handles btnExponetialFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserEntry.Text)
+
+        strResult = dblAnswer.ToString("e")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub btnCurrencyFormat_Click(sender As Object, e As EventArgs) Handles btnCurrencyFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserEntry.Text)
+
+        strResult = dblAnswer.ToString("c")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub btnPercentFormat_Click(sender As Object, e As EventArgs) Handles btnPercentFormat.Click
+        Dim dblAnswer As Double
+        Dim strResult As String
+
+        dblAnswer = CDbl(txtUserEntry.Text)
+
+        strResult = dblAnswer.ToString("p")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub btnShortDate_Click(sender As Object, e As EventArgs) Handles btnShortDate.Click
+        Dim dtmAnswer As Date
+        Dim strResult As String
+
+        dtmAnswer = CDate(txtUserEntry.Text)
+
+        strResult = dtmAnswer.ToString("d")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub btnLongDate_Click(sender As Object, e As EventArgs) Handles btnLongDate.Click
+        Dim dtmAnswer As Date
+        Dim strResult As String
+
+        dtmAnswer = CDate(txtUserEntry.Text)
+
+        strResult = dtmAnswer.ToString("D")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim dtmAnswer As Date
+        Dim strResult As String
+
+        dtmAnswer = CDate(txtUserEntry.Text)
+
+        strResult = dtmAnswer.ToString("t")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim dtmAnswer As Date
+        Dim strResult As String
+
+        dtmAnswer = CDate(txtUserEntry.Text)
+
+        strResult = dtmAnswer.ToString("T")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim dtmAnswer As Date
+        Dim strResult As String
+
+        dtmAnswer = CDate(txtUserEntry.Text)
+
+        strResult = dtmAnswer.ToString("F")
+        lblResult.Text = strResult
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtUserEntry.Clear()
+        lblResult.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
